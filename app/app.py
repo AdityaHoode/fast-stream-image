ec9881d362a2e89403942b530f57e1a22b3306c6
AdityaHoode <adityahoode3@gmail.com>
2026-03-13 12:51:51 +0530

    post db

--- app/app.py
+++ app/app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
 from app.schemas import PostCreate, PostResponse
+from app.db import Post, create_db_and_tables, get_async_session
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 text_posts = {
     1: {"title": "Post 1", "content": "This is post 1"},
--- app/db.py
+++ app/db.py
@@ -1,15 +1,18 @@
 from collections.abc import AsyncGenerator
 import uuid
-import datetime
+from datetime import datetime
 
 from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.asyncio import AsyncGenerator, AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 
-DATABASE_URL = "sqllite+aiosqllit:///./test.db"
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
-class Post(DeclarativeBase):
+class Base(DeclarativeBase):
+    pass
+
+class Post(Base):
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -24,7 +27,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
