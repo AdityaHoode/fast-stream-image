7985be95c934751151132104cf1501b66d17a83f
AdityaHoode <adityahoode3@gmail.com>
2026-03-20 13:54:37 +0530

    implement crud

--- app/app.py
+++ app/app.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+# from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
+import shutil
+import os
+import uuid
+import tempfile
 
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
+from app.images import imageKit
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,18 +24,36 @@ async def upload_file(
     file: UploadFile = File(...),
     caption: str = Form(""),
     session: AsyncSession = Depends(get_async_session)
-): 
-    post = Post(
-        caption=caption,
-        url="dummy url",
-        file_type="photo",
-        file_name="dummy name",
-    )
-    session.add(post)
-    await session.commit()
-    await session.refresh(post)
-    
-    return post
+):
+    try:
+        file_bytes = await file.read()
+
+        upload_result = imageKit.files.upload(
+            file=file_bytes,
+            file_name=file.filename,
+            use_unique_file_name=True,
+            tags=["backend-upload"]
+        )
+
+        if upload_result:
+            post = Post(
+                caption=caption,
+                url=upload_result.url,
+                file_type=upload_result.file_type,
+                file_name=upload_result.name,
+            )
+
+            session.add(post)
+            await session.commit()
+            await session.refresh(post)
+
+            return post
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        await file.close()
 
 @app.get("/feed")
 async def get_feed(
@@ -52,3 +76,21 @@ async def get_feed(
         )
 
     return {"posts": posts_data}
+
+@app.delete("/posts/{post_id}")
+async def delete_post(post_id: str, session: AsyncSession = Depends(get_async_session)):
+    try:
+        post_uuid = uuid.UUID(post_id)
+
+        result = await session.execute(select(Post).where(Post.id == post_uuid))
+        post = result.scalars().first()
+
+        if not post:
+            HTTPException(status_code=400, detail="Post not found")
+
+        await session.delete(post)
+        await session.commit()
+
+        return {"success": True, "message": "Post deleted successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ app/images.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+from imagekitio import ImageKit
+
+load_dotenv()
+
+imageKit = ImageKit(private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"))
+
